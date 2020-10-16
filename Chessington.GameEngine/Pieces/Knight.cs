c60484d33d42e97b4907b35ed83da4bf049c6df7
FraMa <franky.ma24@gmail.com>
2020-10-16 16:32:34 +0100

    Added movement for knights

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            var moves = new HashSet<Square>();
+
+            for (var row = 0; row < 8; row++)
+            {
+                for (var col = 0; col < 8; col++)
+                {
+                    var rowDiff = Math.Abs(position.Row - row);
+                    var colDiff = Math.Abs(position.Col - col);
+
+                    if (rowDiff + colDiff == 3 && rowDiff * colDiff != 0)
+                    {
+                        moves.Add(Square.At(row, col));
+                    }
+                }
+            }
+
+            return moves;
         }
     }
 }
